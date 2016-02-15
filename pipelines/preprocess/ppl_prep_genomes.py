f1b6dc9d118c50bf733121afd595c6fd7159a3a1
Peter Ebert <pebert@mpi-inf.mpg.de>
2016-02-15 13:51:44 +0100

    Pipeline to preprocess genome files is in working state; needs to be extended to include conversion into HDF5 format

--- pipelines/preprocess/ppl_prep_genomes.py
+++ pipelines/preprocess/ppl_prep_genomes.py
@@ -1,11 +1,19 @@
 # coding=utf-8
 
+import os as os
+
 from ruffus import *
 
+
 def build_pipeline(args, config, sci_obj):
+    """
+    :param args:
+    :param config:
+    :param sci_obj:
+    :return:
+    """
 
-
-    pipe = Pipeline(name='PREP_GENOMES')
+    pipe = Pipeline(name=config.get('Pipeline', 'name'))
 
     sci_obj.set_config_env(dict(config.items('JobConfig')), dict(config.items('EnvConfig')))
     if args.gridmode:
@@ -13,5 +21,40 @@ def build_pipeline(args, config, sci_obj):
     else:
         jobcall = sci_obj.ruffus_localjob()
 
+    tempdir = config.get('Pipeline', 'tempdir')
+    outdir = config.get('Pipeline', 'outdir')
+
+    inputfiles = []
+    for path in config.get('Pipeline', 'inputfiles').split(':'):
+        allentries = os.listdir(path)
+        for entry in allentries:
+            fpath = os.path.join(path, entry)
+            if os.path.isfile(fpath):
+                inputfiles.append(fpath)
+
+    # step 0: initiate pipeline with input files
+    init = pipe.originate(task_func=lambda x: x, output=inputfiles)
+
+    cmd = config.get('Pipeline', 'filtchrom')
+    # step 1: select only autosomes from chrom sizes file
+    filtchrom = pipe.transform(task_func=sci_obj.get_jobf('in_out'),
+                               name='filtchrom',
+                               input=output_from(init),
+                               filter=suffix('.chrom.sizes'),
+                               output='.selected',
+                               output_dir=tempdir,
+                               extras=[cmd, jobcall]).mkdir(tempdir)
+
+    cmd = config.get('Pipeline', 'conv2bit')
+    # step 2: convert 2bit files into gzipped fasta files
+    conv2bit = pipe.collate(task_func=sci_obj.get_jobf('inref_out'),
+                            name='conv2bit',
+                            input=output_from(init, filtchrom),
+                            filter=formatter('(?P<ASSEMBLY>\w+)\.(2bit|selected)'),
+                            output=os.path.join(tempdir, '{ASSEMBLY[0]}.fa.gz'),
+                            extras=[cmd, '.selected', jobcall])
+
+    # step 3: convert fasta files into HDF5 files
+    #convert_fasta = ruf.transform()
 
     return pipe
