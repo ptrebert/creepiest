After updating target indices and fixing bugs in the prep_encode pipeline, full proc_encode run was initiated. Completed w/o errors including calculation of pairwise correlations of signal tracks. State of pipeline should be considered fix by now.

crplib/auxiliary/hdf_ops.py
@@ -81,7 +81,7 @@ def build_conservation_mask(chainfile, chrom, csize=None):
         mask = np.ones(chromsize, dtype=np.bool)
     opn, mode = text_file_mode(chainfile)
     num_aln = 0
-    with opn(chainfile, mode) as cf:
+    with opn(chainfile, mode=mode, encoding='ascii') as cf:
         chainit = get_chain_iterator(cf, tselect=chrom)
         for aln in chainit:
             mask[aln[1]:aln[2]] = 0

crplib/commands/apply.py
@@ -31,11 +31,17 @@ def smooth_signal_estimate(signal, res):
     # for default resolution of 25, spans roughly one nucleosome
     window = res * 10
     for pos in range(CHROMOSOME_BOUNDARY, len(signal), window):
-        smoother = kspline(range(pos, pos+window),  # x-axis
-                           signal[pos:pos+window],  # y-axis
-                           t=[j+res for j in range(pos, pos + window - res, res)],  # knots
-                           k=3)  # degree of polynomial, cubic is default
-        signal[pos:pos+window] = smoother(range(pos, pos+window))
+        try:
+            smoother = kspline(range(pos, pos+window),  # x-axis
+                               signal[pos:pos+window],  # y-axis
+                               t=[j+res for j in range(pos, pos + window - res, res)],  # knots
+                               k=3)  # degree of polynomial, cubic is default
+            signal[pos:pos+window] = smoother(range(pos, pos+window))
+        except Exception as err:
+            if pos + window > len(signal):
+                break
+            else:
+                raise err
     signal = np.clip(signal, 0, signal.max())
     return signal
 
@@ -46,7 +52,6 @@ def make_signal_estimate(params):
     :return:
     """
     chrom = params['chrom']
-    mypid = mp.current_process().pid
     model = pck.load(open(params['modelfile'], 'rb'))
     seq = get_twobit_seq(params['seqfile'], params['chrom'])
     chromlen = len(seq)
@@ -80,9 +85,12 @@ def make_signal_estimate(params):
         y_hat = model.predict(np.array(chunks))
         for idx, val in zip(positions, y_hat):
             est_sig[idx:idx+res] = val
+    print('Smoothing {}'.format(chrom))
     if not params['nosmooth']:
         est_sig = smooth_signal_estimate(est_sig, res)
-    return mypid, params['chrom'], est_sig
+    print(chrom)
+    print(type(est_sig))
+    return chrom, est_sig
 
 
 def assemble_params_estsig(args):
@@ -118,15 +126,13 @@ def run_estimate_signal(logger, args):
     """
     logger.debug('Assembling worker parameters')
     arglist = assemble_params_estsig(args)
-    with pd.HDFStore(args.outputfile, 'w', complevel=9, complib='blosc') as hdfout:
-        if 'metadata' in hdfout:
-            metadata = hdfout['metadata']
-        else:
-            metadata = pd.DataFrame(columns=MD_SIGNAL_COLDEFS)
+    logger.debug('Created parameter list of size {}'.format(len(arglist)))
+    with pd.HDFStore(args.outputfile, 'w', complevel=9, complib='blosc', encoding='utf-8') as hdfout:
+        metadata = pd.DataFrame(columns=MD_SIGNAL_COLDEFS)
         with mp.Pool(args.workers) as pool:
             logger.debug('Start processing...')
-            mapres = pool.imap_unordered(make_signal_estimate, arglist, chunksize=1)
-            for chrom, valobj in mapres:
+            resit = pool.imap_unordered(make_signal_estimate, arglist, chunksize=1)
+            for chrom, valobj in resit:
                 logger.debug('Processed chromosome {}'.format(chrom))
                 group, valobj, metadata = gen_obj_and_md(metadata, args.outputgroup, chrom, args.inputfile, valobj)
                 hdfout.put(group, valobj, format='fixed')

crplib/commands/correlation.py
@@ -11,9 +11,8 @@ import numpy as np
 import pandas as pd
 import json as json
 
-from crplib.auxiliary.hdf_ops import get_valid_hdf5_groups, get_trgindex_groups, load_masked_sigtrack, build_conservation_mask
-from crplib.numalg.iterators import iter_consecutive_blocks
-
+from crplib.auxiliary.hdf_ops import get_valid_hdf5_groups, get_trgindex_groups
+from crplib.auxiliary.file_ops import create_filepath
 
 def assemble_worker_params(args):
     """
@@ -67,7 +66,7 @@ def compute_corr_cons(params):
     :param params:
     :return:
     """
-    mypid = mp.current_process().pid
+    chrom = params['chrom']
     # loading index data
     fun_avg = np.vectorize(np.average, otypes=[np.float64])
     with pd.HDFStore(params['targetindex'], 'r') as idx:
@@ -89,7 +88,7 @@ def compute_corr_cons(params):
             corr, pv = res, -1
         infos = {'stat': corr, 'pv': pv}
         results[ms] = infos
-    return mypid, params['chrom'], results
+    return chrom, results
 
 
 def compute_corr_active(params):
@@ -98,7 +97,6 @@ def compute_corr_active(params):
     :param params:
     :return:
     """
-    mypid = mp.current_process().pid
     with pd.HDFStore(params['inputfilea'], 'r') as hdf:
         load_group = os.path.join(params['inputgroupa'], params['chrom'])
         data = hdf[load_group].values
@@ -120,7 +118,7 @@ def compute_corr_active(params):
             corr, pv = res, -1
         infos = {'stat': corr, 'pv': pv.data}
         results[ms] = infos
-    return mypid, params['chrom'], results
+    return params['chrom'], results
 
 
 def compute_corr_full(params):
@@ -128,7 +126,6 @@ def compute_corr_full(params):
     :param params:
     :return:
     """
-    mypid = mp.current_process().pid
     with pd.HDFStore(params['inputfilea'], 'r') as hdf:
         load_group = os.path.join(params['inputgroupa'], params['chrom'])
         dataset1 = hdf[load_group].values
@@ -145,7 +142,7 @@ def compute_corr_full(params):
             corr, pv = res, -1
         infos = {'stat': corr, 'pv': pv}
         results[ms] = infos
-    return mypid, params['chrom'], results
+    return params['chrom'], results
 
 
 def run_compute_correlation(args):
@@ -163,7 +160,6 @@ def run_compute_correlation(args):
     output = {'file_A': os.path.basename(args.inputfilea),
               'file_B': os.path.basename(args.inputfileb),
               'targetindex': 'None' if not args.targetindex else os.path.basename(args.targetindex),
-              'indexgroup': 'None' if not args.indexgroup else args.indexgroup,
               'group_A': args.inputgroupa, 'group_B': args.inputgroupb,
               'task': args.task, 'measure': args.measure,
               'correlations': []}
@@ -174,7 +170,8 @@ def run_compute_correlation(args):
             logger.debug('Computed correlation for chromosome {}'.format(chrom))
             output['correlations'].append((chrom, results))
     logger.debug('Finished computation')
-    with open(args.outputfile, 'w') as outfile:
+    fpath = create_filepath(args.outputfile, logger)
+    with open(fpath, 'w') as outfile:
         json.dump(output, outfile, indent=1, sort_keys=True)
     logger.debug('Output written to file {}'.format(args.outputfile))
     return 0