3383ad645c615f7d0fc3321222862e758109e7ff
Peter Ebert <pebert@mpi-inf.mpg.de>
2016-05-18 11:03:43 +0200

    If mappable signal (msig) feature is included in the training data, srcfile refers to both input file and the signal file (comma separated entry)

--- crplib/metadata/md_traindata.py
+++ crplib/metadata/md_traindata.py
@@ -36,8 +36,12 @@ def gen_obj_and_md(mdframe, group, chrom, args, datavals):
     features = ','.join(args.features)
     kmers = ','.join(map(str, args.kmers))
     size_mem = (dataobj.values.nbytes + dataobj.index.nbytes) / DIV_B_TO_MB
+    if 'msig' in args.features:
+        srcfiles = os.path.basename(args.inputfile) + ',' + os.path.basename(args.signalfile)
+    else:
+        srcfiles = os.path.basename(args.inputfile)
     entries = [group, chrom, mtime, int(size_mem), numsamples, resolution, features,
-               kmers, os.path.basename(args.inputfile), os.path.basename(args.targetindex)]
+               kmers, srcfiles, os.path.basename(args.targetindex)]
     upd_idx = update_metadata_index(mdframe, group)
     if upd_idx is not None:
         mdframe.iloc[upd_idx, ] = entries
