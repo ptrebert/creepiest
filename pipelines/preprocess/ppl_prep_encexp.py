72a6af2b40384757b5a0c1fd9477a4925383a8a0
Peter Ebert <pebert@mpi-inf.mpg.de>
2016-06-13 13:53:56 +0200

    ENH: After discovering that ENCODE has not released expression files quantifying genes (i.e. GTF) for the relevant mm9 data, added the raw data (FASTQ) as new data source. Gene expression estimates are produced with Salmon (Cufflinks is dead). Probably should process the human data in the same way when I find the time.

--- pipelines/preprocess/ppl_prep_encexp.py
+++ pipelines/preprocess/ppl_prep_encexp.py
@@ -4,12 +4,21 @@ import os as os
 import csv as csv
 import gzip as gz
 import re as re
+import io as io
 import collections as col
+import fnmatch as fnm
 import json as js
 import sys as sys
 import numpy as np
 
 from ruffus import *
+
+# This is from GENCODE, based on the description of the transcriptome fasta files:
+# Protein-coding transcript sequences
+# Transcript biotypes: protein_coding, nonsense_mediated_decay,
+# non_stop_decay, IG_*_gene, TR_*_gene, polymorphic_pseudogene
+ACCEPT_TRANSCRIPT = {'protein_coding', 'nonsense_mediated_decay', 'non_stop_decay', 'polymorphic_pseudogene'}
+# plus the following: IG_*_gene, TR_*_gene
 
 
 def link_encode_files(mdfile, indir, trgdir):
@@ -93,6 +102,38 @@ def _parse_gtf_line(line):
     return chrom, int(start), int(end), strand, infos
 
 
+def link_fastq_files(metadata, indir, outdir):
+    """
+    :param indir:
+    :param outdir:
+    :param metadata:
+    :return:
+    """
+    from preprocess.__init__ import ENCODE_LAB_MAP as LAB_MAP
+
+    all_files = os.listdir(indir)
+    bam_files = fnm.filter(all_files, '*.fastq.gz')
+    fp_map = dict((fn, os.path.join(indir, fn)) for fn in bam_files)
+
+    linked = []
+    with open(metadata, 'r') as mdfile:
+        rows = csv.DictReader(mdfile, delimiter='\t')
+        for r in rows:
+            if not bool(int(r['use'])):
+                continue
+            fn = os.path.basename(r['fastq'])
+            encff = fn.split('.')[0]
+            old_path = fp_map[fn]
+            new_name = '_'.join([r['experiment'], encff, r['assembly'], r['cell'],
+                                 'mRNA', LAB_MAP[r['lab'].strip('"')], 'R' + str(r['rep'])])
+            new_name += '.se.fastq.gz'
+            new_path = os.path.join(outdir, new_name)
+            if not os.path.islink(new_path):
+                os.link(old_path, new_path)
+            linked.append(new_path)
+    return linked
+
+
 def convert_gtf(inpath, outpath):
     """
     :param inpath:
@@ -100,7 +141,7 @@ def convert_gtf(inpath, outpath):
     :return:
     """
     genes = dict()
-    chrom_match = re.compile('chr[0-9]+')
+    chrom_match = re.compile('chr[0-9XY]+')
     with gz.open(inpath, 'rb') as gtf:
         for line in gtf:
             line = line.decode('ascii').strip()
@@ -111,7 +152,14 @@ def convert_gtf(inpath, outpath):
                 continue
             if parts[2] == 'gene':
                 c, s, e, strand, infos = _parse_gtf_line(line)
-                if infos['gene_type'] != 'protein_coding':
+                biotype = infos['gene_type']
+                if biotype in ACCEPT_TRANSCRIPT:
+                    pass
+                elif biotype.startswith('IG_') and biotype.endswith('_gene'):
+                    pass
+                elif biotype.startswith('TR_') and biotype.endswith('_gene'):
+                    pass
+                else:
                     continue
                 this_gene = {'chrom': c, 'start': s, 'end': e, 'symbol': infos['gene_name'],
                              'ensid': infos['gene_id'], 'transcripts': [], 'strand': strand}
@@ -129,10 +177,58 @@ def convert_gtf(inpath, outpath):
                 if gid not in genes:
                     continue
                 genes[gid]['transcripts'].append(this_trans)
-    genes = sorted(genes.values(), key=lambda d: (int(d['chrom'].strip('chr')), d['start'], d['end']))
+    assert genes, 'No genes selected from GTF file'
+    genes = sorted(genes.values(), key=lambda d: (d['chrom'], d['start'], d['end']))
     with open(outpath, 'w') as outf:
         _ = js.dump(genes, outf, indent=1)
     return outpath
+
+
+def create_gtf_white_black_list(inputfile, outputfiles):
+    """ Create a white- and blacklist from an input GTF file following
+    as much as possible the definitions used by GENCODE for gene model FASTA files
+    see: http://www.gencodegenes.org/releases/
+    :param inputfile:
+    :param outputfiles:
+    :return:
+    """
+    white = io.StringIO()
+    black = io.StringIO()
+    chrom_re = re.compile("chr[0-9XY]+$")
+    trans_re = re.compile("\stranscript_type\s\"(?P<TRANSTYPE>\w+)\";\s")
+    with gz.open(inputfile, 'r') as gtf:
+
+        for line in gtf:
+            line = line.decode('utf-8')
+            if line.startswith('#'):
+                white.write(line)
+                black.write(line)
+                continue
+            elif not line:
+                continue
+            else:
+                cols = line.strip().split('\t')
+                if cols[2] == 'transcript':
+                    if chrom_re.match(cols[0]) is None:
+                        black.write(line)
+                        continue
+                    else:
+                        mobj = trans_re.search(cols[8])
+                        assert mobj is not None, 'Could not identify transcript type in line: {}'.format(line)
+                        ttype = mobj.group('TRANSTYPE').strip()
+                        if ttype in ACCEPT_TRANSCRIPT:
+                            white.write(line)
+                        elif ttype.startswith('IG_') and ttype.endswith('_gene'):
+                            white.write(line)
+                        elif ttype.startswith('TR_') and ttype.endswith('_gene'):
+                            white.write(line)
+                        else:
+                            black.write(line)
+    with open(outputfiles[0], 'w') as out:
+        _ = out.write(white.getvalue())
+    with open(outputfiles[1], 'w') as out:
+        _ = out.write(black.getvalue())
+    return outputfiles
 
 
 def merge_expquant_files(inputfiles, outputfile, genemodel):
@@ -182,6 +278,88 @@ def merge_expquant_files(inputfiles, outputfile, genemodel):
     return outputfile
 
 
+def dump_gene_regions(inputfile, outputfile, regtype):
+    """
+    :param inputfile:
+    :param outputfile:
+    :param regtype:
+    :return:
+    """
+    with open(inputfile, 'r') as infile:
+        genes = js.load(infile)
+    outbuf = io.StringIO()
+    # NB: by construction, genes are sorted
+    # by genomic coordinate
+    for gene in genes:
+        start, end = gene[regtype]
+        _ = outbuf.write('\t'.join([gene['chrom'], str(start), str(end), gene['ensid']]) + '\n')
+    with open(outputfile, 'w') as outfile:
+        _ = outfile.write(outbuf.getvalue())
+    return outputfile
+
+
+def make_musbed_params(quantdir, fastqdir, outdir, gencodefile):
+    """
+    :param quantdir:
+    :param fastqdir:
+    :param outdir:
+    :param gencodefile:
+    :return:
+    """
+    gencver = gencodefile.split('_')[1]
+    all_files = os.listdir(fastqdir)
+    all_fastq = fnm.filter(all_files, '*.fastq.gz')
+    arglist = []
+    for root, dirs, files in os.walk(quantdir):
+        if files:
+            for f in files:
+                if f == 'quant.sf':
+                    _, encexp = os.path.split(root)
+                    expfiles = fnm.filter(all_fastq, '*' + encexp + '*')
+                    assert len(expfiles) == 2, 'Missing files for ENCODE experiment: {}'.format(encexp)
+                    expfile = expfiles[0]
+                    components = expfile.split('.')[0].split('_')
+                    new_name = '_'.join([encexp, components[2], components[3], 'mRNA', components[5]]) + '.genc-' + gencver + '.bed'
+                    new_path = os.path.join(outdir, new_name)
+                    arglist.append([os.path.join(root, f), new_path, gencodefile])
+    return arglist
+
+
+def salmon_to_bed_genes(inputfile, outputfile, gencode):
+    """
+    :param inputfile:
+    :param outputfile:
+    :param gencode:
+    :return:
+    """
+    gene_expression = col.defaultdict(float)
+    with open(inputfile, 'r') as inf:
+        rows = csv.DictReader(inf, delimiter='\t')
+        for r in rows:
+            name_parts = r['Name'].split('|')
+            gene_symbol = name_parts[5]
+            if gene_symbol.startswith('mt-'):
+                # ignore chrM genes
+                continue
+            tpm = float(r['TPM'])
+            gene_expression[gene_symbol] += tpm
+
+    annotation = js.load(open(gencode, 'r'))
+    annotation = dict((g['symbol'], (g['chrom'], g['start'], g['end'], g['ensid'])) for g in annotation)
+    outbuf = []
+    for symbol, tpm in gene_expression.items():
+        chrom, start, end, ensid = annotation[symbol]
+        gene_region = chrom, start, end, ensid, symbol, str(tpm)
+        outbuf.append(gene_region)
+
+    outbuf = sorted(outbuf, key=lambda x: (x[0], x[1], x[2]))
+    with open(outputfile, 'w') as outf:
+        _ = outf.write('\t'.join(['#chrom', 'start', 'end', 'ensid', 'symbol', 'expr']) + '\n')
+        outbuf = ['\t'.join(map(str, gene)) for gene in outbuf]
+        _ = outf.write('\n'.join(outbuf) + '\n')
+    return outputfile
+
+
 def build_pipeline(args, config, sci_obj):
     """
     :param args:
@@ -200,14 +378,18 @@ def build_pipeline(args, config, sci_obj):
 
     refdir = config.get('Pipeline', 'refdir')
     tempdir = config.get('Pipeline', 'tempdir')
+    tmpquant = config.get('Pipeline', 'tmpquant')
     bedout = config.get('Pipeline', 'bedout')
     hdfout = config.get('Pipeline', 'hdfout')
     datadir = config.get('Pipeline', 'datadir')
 
     enc_mdfile = config.get('Pipeline', 'encmd')
+    bam_mdfile = config.get('Pipeline', 'bammd')
 
     inputfiles = [os.path.join(refdir, f) for f in os.listdir(refdir)]
     tmpf = link_encode_files(enc_mdfile, datadir, tempdir)
+    inputfiles.extend(tmpf)
+    tmpf = link_fastq_files(bam_mdfile, datadir, tempdir)
     inputfiles.extend(tmpf)
 
     # step 0: initiate pipeline with input files
@@ -216,8 +398,8 @@ def build_pipeline(args, config, sci_obj):
     convgenc = pipe.transform(task_func=convert_gtf,
                               name='convgenc',
                               input=output_from(init),
-                              filter=formatter('gencode\.(?P<GENCVER>v[0-9a-zA-Z]+)\.+'),
-                              output=os.path.join(refdir, 'gencode_{GENCVER[0]}_main.json')).jobs_limit(2)
+                              filter=formatter('gencode\.(?P<GENCVER>v[0-9a-zA-Z]+)\.annotation\.(?P<ASSEMBLY>\w+)\.+'),
+                              output=os.path.join(refdir, 'gencode_{GENCVER[0]}_{ASSEMBLY[0]}_main.json')).jobs_limit(2)
 
     mergequant = pipe.collate(task_func=merge_expquant_files,
                               name='mergequant',
@@ -225,6 +407,63 @@ def build_pipeline(args, config, sci_obj):
                               # ENCSR000COQ_ENCFF000EWE_hg19_GM12878_mRNA_L13_R0.genc-v7.gtf.gz
                               filter=formatter('(?P<ENCEXP>\w+)_(?P<ENCF>\w+)_(?P<ASS>\w+)_(?P<CELL>\w+)_mRNA_(?P<LAB>\w+)_(?P<REP>\w+)\.genc-(?P<GENCVER>\w+)\.gtf\.gz'),
                               output=os.path.join(bedout, '{ENCEXP[0]}_{ASS[0]}_{CELL[0]}_mRNA_{LAB[0]}.genc-{GENCVER[0]}.bed'),
-                              extras=[os.path.join(refdir, 'gencode_{GENCVER[0]}_main.json')]).mkdir(bedout).jobs_limit(1)
+                              extras=[os.path.join(refdir, 'gencode_{GENCVER[0]}_{ASS[0]}_main.json')]).mkdir(bedout).jobs_limit(1)
+
+    promdump = pipe.transform(task_func=dump_gene_regions,
+                              name='promdump',
+                              input=output_from(convgenc),
+                              filter=suffix('.json'),
+                              output='.2kbprom.bed',
+                              output_dir=refdir,
+                              extras=['promoter']).jobs_limit(2)
+
+    bodydump = pipe.transform(task_func=dump_gene_regions,
+                              name='bodydump',
+                              input=output_from(convgenc),
+                              filter=suffix('.json'),
+                              output='.body.bed',
+                              output_dir=refdir,
+                              extras=['body']).jobs_limit(2)
+
+    cmd = config.get('Pipeline', 'convdump')
+    convdump = pipe.transform(task_func=sci_obj.get_jobf('in_out'),
+                              name='convdump',
+                              input=output_from(promdump, bodydump),
+                              filter=formatter('gencode_(?P<GENCVER>\w+)_(?P<ASSEMBLY>\w+)_main\.(?P<REGTYPE>\w+)\.bed'),
+                              output=os.path.join(refdir, 'gencode_{GENCVER[0]}_{ASSEMBLY[0]}_main.{REGTYPE[0]}.h5'),
+                              extras=[cmd, jobcall])
+
+    makelists = pipe.subdivide(task_func=create_gtf_white_black_list,
+                               name='makelists',
+                               input=output_from(init),
+                               filter=formatter('gencode\.(?P<GENCVER>v[0-9a-zA-Z]+)\.annotation\.(?P<ASSEMBLY>\w+)\.+'),
+                               output=[os.path.join(refdir, 'gencode_{GENCVER[0]}_{ASSEMBLY[0]}_pctranscripts_white.gtf'),
+                                       os.path.join(refdir, 'gencode_{GENCVER[0]}_{ASSEMBLY[0]}_pctranscripts_black.gtf')]).jobs_limit(2)
+
+    sci_obj.set_config_env(dict(config.items('ParallelJobConfig')), dict(config.items('EnvConfig')))
+    if args.gridmode:
+        jobcall = sci_obj.ruffus_gridjob()
+    else:
+        jobcall = sci_obj.ruffus_localjob()
+
+    cmd = config.get('Pipeline', 'salmonidx')
+    salmonidx = pipe.transform(task_func=sci_obj.get_jobf('in_out'),
+                               name='salmonidx',
+                               input=output_from(init),
+                               filter=formatter('(?P<TRANSCRIPTOME>[\w\.]+)\.fa\.gz$'),
+                               output=os.path.join(refdir, '{TRANSCRIPTOME[0]}.idx', 'hash.bin'),
+                               extras=[cmd, jobcall])
+
+    cmd = config.get('Pipeline', 'musquant').replace('\n', ' ')
+    musquant = pipe.collate(task_func=sci_obj.get_jobf('ins_out'),
+                            name='musquant',
+                            input=output_from(init),
+                            filter=formatter('(?P<ENCEXP>\w+)_(?P<ENCF>\w+)_(?P<ASS>\w+)_(?P<CELL>\w+)_mRNA_(?P<LAB>\w+)_(?P<REP>\w+)\.se\.fastq'),
+                            output=os.path.join(tmpquant, '{ENCEXP[0]}', 'quant.sf'),
+                            extras=[cmd, jobcall]).mkdir(tmpquant).follows(salmonidx)
+
+    musbed_params = make_musbed_params(tmpquant, tempdir, bedout, os.path.join(refdir, 'gencode_vM1_mm9_main.json'))
+
+    musbed = pipe.files(salmon_to_bed_genes, musbed_params, name='musbed').jobs_limit(2).follows(musquant).follows(convgenc)
 
     return pipe
