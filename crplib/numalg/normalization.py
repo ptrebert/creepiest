f94a058854fe14bf4bb571a2a121c4870ac860e8
Peter Ebert <pebert@mpi-inf.mpg.de>
2017-02-08 15:03:17 +0100

    DEP: deprecated resource intensive implementation of quantile normalization

--- crplib/numalg/normalization.py
+++ crplib/numalg/normalization.py
@@ -1,55 +1,9 @@
 # coding=utf-8
 
-import pandas as pd
 import numpy as np
 import numpy.random as rng
 import numpy.ma as msk
 import scipy.stats as stats
-
-
-def quant_norm_deprecated(*args, statistic='mean', nzonly=False):
-    """ Perform quantile normalization as proposed by Bolstad et al.
-    Since the handling of ties during the ranking is not specified in the original paper,
-    this method uses dense ranking (rank always increases by 1 between groups)
-    Hence, the result of this function is slightly different to, e.g., the R function
-    normalize.quantiles from the preprocessCore package
-
-    :param args:
-     :type: numpy.ndarray, tuple of numpy.ndarray
-    :param statistic: which statistic to use; default: mean
-     :type: str
-    :param nzonly: reduce data to columns where at least one sample has a non-zero entry
-    :return: the quantile normalized data
-     :rtype: pandas.core.frame.DataFrame
-    """
-    #
-    # This method consumes too much memory (Pandas overhead???)
-    # Use only for small data sizes (num data points < 10e6)
-    #
-    # dataframe with samples (rows) X values (cols)
-    # avoid: "can use starred expression only as assignment target"
-    # df = pd.DataFrame([*args], dtype='float64')
-    df = pd.DataFrame([arg for arg in args], dtype='float64')
-    if nzonly:
-        raise NotImplementedError('Using only non-zero columns not yet implemented')
-    # compute ranks for each sample
-    rk = df.rank(axis=1, method='dense')
-    # sort values in each sample (row)
-    df = df.apply(np.sort, axis=1, raw=True)
-    # generate series with data (statistic of values, for each col)
-    # and index (= ranking of statistic)
-    if statistic == 'mean':
-        col_stats = pd.Series(df.mean(axis=0).values, index=df.mean(axis=0).rank(method='dense'))
-    elif statistic == 'median':
-        col_stats = pd.Series(df.median(axis=0).values, index=df.median(axis=0).rank(method='dense'))
-    else:
-        raise ValueError('Unknown statistic: {}'.format(statistic))
-    # sort for faster access
-    # ...could be premature optimization here...
-    col_stats.sort_index(inplace=True)
-    # replace rank values with averages
-    rk.replace(to_replace=col_stats.index, value=col_stats.values, inplace=True)
-    return rk
 
 
 def nonzero_qnorm(mat):
