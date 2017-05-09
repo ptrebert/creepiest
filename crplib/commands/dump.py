45695ed44e052afc5e3c2b8085d4486206553778
Peter Ebert <pebert@mpi-inf.mpg.de>
2017-05-09 13:46:09 +0200

    ENH: added code to dump region data; still missing: signal and feature datasets

--- crplib/commands/dump.py
+++ crplib/commands/dump.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 
 from crplib.auxiliary.file_ops import text_file_mode
-from crplib.auxiliary.hdf_ops import get_valid_hdf5_groups
+from crplib.auxiliary.hdf_ops import get_valid_hdf5_groups, determine_crp_filetype
 
 
 def set_output(outfile):
@@ -110,6 +110,77 @@ def dump_signal(args, logger):
     raise NotImplementedError()
 
 
+def rearrange_columns(dataset, chrom_prefix):
+    """
+    :param dataset:
+    :return:
+    """
+    idx_count = 9
+    known_cols = {'chrom': 0, 'start': 1, 'end': 2, 'name': 3,
+                  'score': 4, 'strand': 5, 'signalValue': 6,
+                  'pValue': 7, 'qValue': 8}
+    col_order = []
+    for c in dataset.columns:
+        try:
+            idx = known_cols[c]
+            col_order.append((idx, c))
+        except KeyError:
+            col_order.append((idx_count, c))
+            idx_count += 1
+    col_order = sorted(col_order, key=lambda x: x[0])
+    col_order = [x[1] for x in col_order]
+    dataset = dataset[col_order]
+    if chrom_prefix:
+        prefix_cols = dataset.columns.tolist()
+        prefix_cols[0] = '#chrom'
+        dataset.columns = prefix_cols
+    return dataset
+
+
+def dump_regions(args, logger):
+    """
+    :param args:
+    :param logger:
+    :return:
+    """
+    load_groups = get_valid_hdf5_groups(args.inputfile, args.inputgroup)
+    dump = []
+    with pd.HDFStore(args.inputfile, 'r') as hdf:
+        for lg in load_groups:
+            _, chrom_name = lg.rsplit('/', 1)
+            data = hdf[lg]
+            data['chrom'] = chrom_name
+            assert 'crp_group_index' not in data.columns, 'Column name duplicate: crp_group_index'
+            data['crp_group_index'] = data.index.tolist()
+            assert 'crp_group_path' not in data.columns, 'Column name duplicate: crp_group_path'
+            data['crp_group_path'] = lg
+            dump.append(data)
+    logger.debug('Concatenating all data groups...')
+    dump = pd.concat(dump, axis=0, join='outer', ignore_index=True)
+    dump.sort_values(by=['chrom', 'start', 'end'], inplace=True)
+    dump.reset_index(drop=True, inplace=True)
+    logger.debug('Final dataset size: {}'.format(dump.shape))
+    dump = rearrange_columns(dump, args.commentheader)
+    col_delim = args.delimiter.strip('"')
+    if args.outputfile in ['stdout', '-']:
+        logger.debug('Dumping data to stdout')
+        out = sys.stdout
+    else:
+        logger.debug('Dumping data to file')
+        out = args.outputfile
+    if args.noheader and not args.rtable:
+        dump.to_csv(out, sep=col_delim, header=False, index=False)
+    elif args.noheader and args.rtable:
+        dump.to_csv(out, sep=col_delim, header=False, index=True, index_label=None)
+    elif not args.noheader and not args.rtable:
+        dump.to_csv(out, sep=col_delim, header=True, index=False, index_label=None)
+    elif not args.noheader and args.rtable:
+        dump.to_csv(out, sep=col_delim, header=True, index=True, index_label=None)
+    else:
+        raise RuntimeError('Impossible combination of parameters...')
+    return
+
+
 def run_dump_data(args):
     """
     :param args:
@@ -120,10 +191,21 @@ def run_dump_data(args):
     if args.mapreference:
         logger.debug('Map reference specified as {} - assuming map index file.'.format(args.mapreference))
         dump_index(args, logger)
-    elif args.summstat:
-        logger.debug('Summary statistic set to {} - assuming signal file.'.format(args.summstat))
-        dump_signal(args, logger)
     else:
-        raise NotImplementedError('Dumping region files not implemented')
-
+        logger.debug('Determining datatype of input file...')
+        dtype = determine_crp_filetype(args.inputfile)
+        if dtype is None:
+            # this implies a valid metadata record that does not match
+            # any of the hard-coded column definitions - possibly, somebody
+            # has been tinkering with this file
+            raise RuntimeError('Could not determine datatype despite valid metadata record.\n'
+                               'Have you manually edited this file? If not, please contact the developer.')
+        elif dtype == 'signal':
+            dump_signal(args, logger)
+        elif dtype == 'regions':
+            dump_regions(args, logger)
+        elif dtype == 'features':
+            raise NotImplementedError('DTYPE {} not implemented'.format(dtype))
+        else:
+            raise ValueError('Unknown datatype in inputfile: {}'.format(dtype))
     return 0
